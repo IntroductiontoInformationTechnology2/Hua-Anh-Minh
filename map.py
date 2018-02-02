3068c670b208975280f5e66ae228264e0e0e3b8b
VARIACION <VARIACION@users.noreply.github.com>
2018-02-02 20:10:20 +0700

    Add files via upload

--- map.py
+++ map.py
@@ -1,24 +1,36 @@
+import winsound
 from turtle import *
 import turtle
 
-turtle.setup(width = 800, height = 600)
-screen = turtle.Screen()
-screen.title("TURTLE RACE")
-length = int(screen.numinput("Setup the map", "Enter the length", 20, 20, 1500))
-width = int(screen.numinput("Setup the map", "Enter the width", 11, 11, 25))
-penup()
-turtle.setpos(-200, 100)
-for step in range(length):
-    write(step, align = 'center')
+winsound.PlaySound('Spectre NCS Release.wav', winsound.SND_ASYNC)
+
+def draw_map():
+    turtle.setup(width = 800, height = 600)
     turtle.speed(0)
-    right(90)
-    for space in range(width):
-        turtle.speed(0)
-        forward(10)
-        pendown()
-        forward(10)
-        penup()
-    backward(220)
-    left(90)
-    forward(20)
-done()
+    screen = turtle.Screen()
+    screen.title("TURTLE RACE")
+    length = int(screen.numinput("Setup the map", "Enter the length", 20, 20, 1500))
+    width = int(screen.numinput("Setup the map", "Enter the width", 11, 11, 25))
+    turtle.penup()
+    turtle.setpos(-200, 100)
+
+    for step in range(int(length / 2)):
+        turtle.write(step, align = 'center')
+        turtle.forward(40)
+    turtle.pensize(5)
+    setY = 100
+    turnback = 1
+    turtle.right(180)
+    setY = setY - 20
+    for i in range(5):
+        turtle.penup()
+        turtle.setpos(180, setY)
+        turtle.pendown()
+        turtle.forward(length * 20)
+        turnback += 1
+        setY = setY - 60
+    turtle.penup()
+    turtle.forward(1000)
+    done()
+
+draw_map()
